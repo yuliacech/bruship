5dbf5a4ca7f25b866998ce0ac5da954e24b259f0
Iuliia <yulia-ichin-norbu@ya.ru>
2018-04-10 17:33:06 +0200

    Change backend to openshift server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseApiUrl: 'https://sheltered-caverns-57536.herokuapp.com',
+  baseApiUrl: 'http://bruship-server-bruship.193b.starter-ca-central-1.openshiftapps.com',
   trackingID: 'UA-111362455-1',
   originUrl: 'http://bruship.solid-flow.com',
   auth0ID: 'ZgWnE2LduQt2MAXrLrpHcCCrKSj7W4Ka'
