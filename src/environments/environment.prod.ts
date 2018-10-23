9dd1ec4e45bd4d1eec32541f8e330d5c631d24c6
Iuliia Ichin-Norbu <iuliia.ichin-norbu@cocomore.com>
2018-10-23 21:36:42 +0200

    Switch to heroku hosten backend server, add angular-cli-ghpages package

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseApiUrl: 'http://bruship-server-bruship.193b.starter-ca-central-1.openshiftapps.com',
+  baseApiUrl: 'https://bruship-server.herokuapp.com',
   trackingID: 'UA-111362455-1',
   originUrl: 'http://bruship.solid-flow.com',
   auth0ID: 'ZgWnE2LduQt2MAXrLrpHcCCrKSj7W4Ka'
--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseApiUrl: 'http://bruship-server-bruship.193b.starter-ca-central-1.openshiftapps.com',
+  baseApiUrl: 'https://bruship-server.herokuapp.com',
   trackingID: 'UA-111362455-1',
   originUrl: 'https://solid-flow.github.io/bruship',
   auth0ID: 'oAxeiYUNgrbz3Q81IES8XWZZ60t_Eqi6'
